f313e3008723e97b7aff6500484e72b3c978807f
adamddh <58528419+adamddh@users.noreply.github.com>
2020-01-20 11:37:47 -0500

    svgD

--- docs/spd.js
+++ docs/spd.js
@@ -1,7 +1,9 @@
-let marginsHM = {top: 30, bottom: 30, left: 30, right: 30}
+let margins = {top: 30, bottom: 30, left: 30, right: 30}
 let outerWidth = 800
 let outerHeight = 450
 
+
+let svgD = d3.select('#adhviz')
 
 
 let svgHM = d3.select('# heat-map')
