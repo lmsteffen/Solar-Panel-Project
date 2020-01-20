58e15a6c90fb9b169c5691e42053a0dfcc3036e7
lms48 <lms48@maroon17.cs.calvin.edu>
2020-01-20 11:59:11 -0500

    created div and added inner height and width

--- docs/spd.js
+++ docs/spd.js
@@ -1,7 +1,13 @@
-let marginsHM = {top: 30, bottom: 30, left: 30, right: 30}
+let margins = {top: 30, bottom: 30, left: 30, right: 30}
 let outerWidth = 800
 let outerHeight = 450
+let innerWidth = outerWidth - margins.left - margins.right
+let innerHeight = outerHeight - margins.top - margins.bottom
 
 
-
-let svgHM = d3.select('# heat-map')
+let svgHM = d3.select('#heat-map')
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
+  .append('g')
+  .attr('id', 'plot-area')
+  .attr('transform', 'translate('+ margins.left + ',' + margins.top ')')
