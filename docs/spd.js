ed79f24d44a21993e72da4616b7ad85ed1f4a59f
adamddh <58528419+adamddh@users.noreply.github.com>
2020-01-20 22:08:18 -0500

    deleted unneccessary items

--- docs/spd.js
+++ docs/spd.js
@@ -1,30 +1,42 @@
-let margins = {top: 30, bottom: 30, left: 30, right: 30}
-let outerWidth = 800
-let outerHeight = 450
-let innerWidth = outerWidth - margins.left - margins.right
-let innerHeight = outerHeight - margins.top - margins.bottom
+let margins = { top: 30, bottom: 30, left: 30, right: 30 };
+let outerWidth = 800;
+let outerHeight = 450;
+let innerWidth = outerWidth - margins.left - margins.right;
+let innerHeight = outerHeight - margins.top - margins.bottom;
 
+let svgHM = d3
+  .select("#heat-map")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight)
+  .append("g")
+  .attr("id", "plot-area");
+//.attr('transform', 'translate('+ margins.left + ',' + margins.top ')') //this line seems to be causing problems -adh
 
-let svgHM = d3.select('#heat-map')
-  .attr('width', outerWidth)
-  .attr('height', outerHeight)
-  // .style('background-color', 'black')
-  // .append('rect')
-  // .attr('x', 0)
-  // .attr('y', 0)
-  // .attr('width', outerWidth)
-  // .attr('height', outerHeight)
-  // .attr('stroke', 'red')
-  .append('g')
-  .attr('id', 'plot-area')
-  .attr('transform', 'translate( ' + margins.left + ',' + margins.top + ')')
+let adhOuter = d3
+  .select("#adhviz")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight);
 
-  d3.csv('ordered-data.csv').then(drawHM)
+let adhInner = adhOuter
+  .append("g")
+  .attr("width", innerWidth)
+  .attr("height", innerHeight)
+  .attr("transform", "translate(" + margins.left + "," + margins.right + ")");
 
-  function drawHM() {
-    svgHM.append('circle')
-    .attr('cx', 200)
-    .attr('cy', 200)
-    .attr('r', 20)
-    .attr('fill', 'red')
-  }
+adhOuter //border
+  .append("rect")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight)
+  .attr("fill", "transparent")
+  .attr("stroke", "#333333")
+  .attr("stroke-width", 2);
+
+/*let wattscale = d3
+  .scaleLinear() // Lauren, this might be useful for you as well
+  .domain( fill this in )
+  .range([0, innerWidth]);
+
+let timescale = d3
+  .scaleLinear()
+  .domain( fill this in later, find out how to make use of timescale )
+  .range([0, innerHeight]) */
