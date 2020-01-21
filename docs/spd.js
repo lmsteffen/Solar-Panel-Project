3f32bd74787649852f79399b0f85ab0d8e05d33e
lms48 <lms48@maroon30.cs.calvin.edu>
2020-01-20 20:47:15 -0500

    loaded data and tested to see if it worked

--- docs/spd.js
+++ docs/spd.js
@@ -1,53 +1,24 @@
-let margins = { top: 30, bottom: 30, left: 30, right: 30 };
-let outerWidth = 800;
-let outerHeight = 450;
-let innerWidth = outerWidth - margins.left - margins.right;
-let innerHeight = outerHeight - margins.top - margins.bottom;
-
-let svgHM = d3
-  .select("#heat-map")
-  .attr("width", outerWidth)
-  .attr("height", outerHeight)
-  .append("g")
-  .attr("id", "plot-area");
-//.attr('transform', 'translate('+ margins.left + ',' + margins.top ')') //this line seems to be causing problems -adh
-
-let adhOuter = d3
-  .select("#adhviz")
-  .attr("width", outerWidth)
-  .attr("height", outerHeight);
-
-let adhInner = adhOuter
-  .append("g")
-  .attr("width", innerWidth)
-  .attr("height", innerHeight)
-  .attr("transform", "translate(" + margins.left + "," + margins.right + ")");
-
-adhOuter //border
-  .append("rect")
-  .attr("width", outerWidth)
-  .attr("height", outerHeight)
-  .attr("fill", "transparent")
-  .attr("stroke", "#333333")
-  .attr("stroke-width", 2);
-
-/*let wattscale = d3
-  .scaleLinear() // Lauren, this might be useful for you as well
-  .domain( fill this in )
-  .range([0, innerWidth]);
-
-let timescale = d3
-  .scaleLinear()
-  .domain( fill this in later, find out how to make use of timescale )
-  .range([0, innerHeight]) */
+let margins = {top: 30, bottom: 30, left: 30, right: 30}
+let outerWidth = 800
+let outerHeight = 450
+let innerWidth = outerWidth - margins.left - margins.right
+let innerHeight = outerHeight - margins.top - margins.bottom
 
 
-  d3.csv('yourcsv.csv')
-    .then( function(data) {
-        console.log('then')
-        console.log(data)
-    })
-    .catch(error =>  {
-      console.log('error')
-      console.log(error)
-    })
+let svgHM = d3.select('div.lms #heat-map')
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
+  .style('background-color', 'white')
+  // .append('g')
+  // .attr('id', 'plot-area')
+  // .attr('transform', 'translate('+ margins.left + ',' + margins.top ')')
+
+  d3.csv('ordered-data.csv').then(draw)
+
+  function draw() {
+    svgHM.append('circle')
+    .attr('cx', 200)
+    .attr('cy', 200)
+    .attr('r', 20)
+    .attr('fill', 'red')
+  }
