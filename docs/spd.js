8f0dba681b23fa5bb1f653c77a854d3b8d527729
adamddh <58528419+adamddh@users.noreply.github.com>
2020-01-20 19:18:49 -0500

    formatting

--- docs/spd.js
+++ docs/spd.js
@@ -1,45 +1,53 @@
-let margins = {top: 30, bottom: 30, left: 30, right: 30}
-let outerWidth = 800
-let outerHeight = 450
-let innerWidth = outerWidth - margins.left - margins.right
-let innerHeight = outerHeight - margins.top - margins.bottom
+let margins = { top: 30, bottom: 30, left: 30, right: 30 };
+let outerWidth = 800;
+let outerHeight = 450;
+let innerWidth = outerWidth - margins.left - margins.right;
+let innerHeight = outerHeight - margins.top - margins.bottom;
 
-
-
-
-let svgHM = d3.select('#heat-map')
-  .attr('width', outerWidth)
-  .attr('height', outerHeight)
-  .append('g')
-  .attr('id', 'plot-area')
-  //.attr('transform', 'translate('+ margins.left + ',' + margins.top ')') //this line seems to be causing problems -adh
-
+let svgHM = d3
+  .select("#heat-map")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight)
+  .append("g")
+  .attr("id", "plot-area");
+//.attr('transform', 'translate('+ margins.left + ',' + margins.top ')') //this line seems to be causing problems -adh
 
 let adhOuter = d3
-  .select('#adhviz')
-  .attr('width', outerWidth)
-  .attr('height', outerHeight)
+  .select("#adhviz")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight);
 
 let adhInner = adhOuter
-  .append('g')
-  .attr('width', innerWidth)
-  .attr('height', innerHeight)
-  .attr('transform', 'translate(' + margins.left + ',' + margins.right + ')')
+  .append("g")
+  .attr("width", innerWidth)
+  .attr("height", innerHeight)
+  .attr("transform", "translate(" + margins.left + "," + margins.right + ")");
 
 adhOuter //border
-    .append('rect')
-    .attr('width', outerWidth)
-    .attr('height', outerHeight)
-    .attr('fill', 'transparent')
-    .attr('stroke', '#333333')
-    .attr('stroke-width', 2)
+  .append("rect")
+  .attr("width", outerWidth)
+  .attr("height", outerHeight)
+  .attr("fill", "transparent")
+  .attr("stroke", "#333333")
+  .attr("stroke-width", 2);
 
-let wattscale = d3
+/*let wattscale = d3
   .scaleLinear() // Lauren, this might be useful for you as well
-  .domain( /*fill this in*/ )
+  .domain( fill this in )
   .range([0, innerWidth]);
 
 let timescale = d3
   .scaleLinear()
-  .domain( /*fill this in later, find out how to make use of timescale */)
-  .range([0, innerHeight])
+  .domain( fill this in later, find out how to make use of timescale )
+  .range([0, innerHeight]) */
+
+
+  d3.csv('yourcsv.csv')
+    .then( function(data) {
+        console.log('then')
+        console.log(data)
+    })
+    .catch(error =>  {
+      console.log('error')
+      console.log(error)
+    })
